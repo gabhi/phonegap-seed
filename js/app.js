2aa5ed07960b3b2c7f5ccca58e69625eada084d5
Abhijit Gaikwad <agaikwad@marketlive.com>
2014-03-16 01:24:06 -0700

    basic summation ability

--- js/app.js
+++ js/app.js
@@ -16,55 +16,54 @@ angular.module('starter', ['ionic', 'starter.services', 'starter.controllers'])
   // Each state's controller can be found in controllers.js
   $stateProvider
 
-    // setup an abstract state for the tabs directive
-    .state('tab', {
-      url: "/tab",
-      abstract: true,
-      templateUrl: "templates/tabs.html"
-    })
+  // setup an abstract state for the tabs directive
+  .state('tab', {
+    url: "/tab",
+    abstract: true,
+    templateUrl: "templates/tabs.html"
+  })
 
-    // the pet tab has its own child nav-view and history
-    .state('tab.pet-index', {
-      url: '/pets',
-      views: {
-        'pets-tab': {
-          templateUrl: 'templates/pet-index.html',
-          controller: 'PetIndexCtrl'
-        }
+  // the pet tab has its own child nav-view and history
+  .state('tab.pet-index', {
+    url: '/pets',
+    views: {
+      'pets-tab': {
+        templateUrl: 'templates/pet-index.html',
+        controller: 'PetIndexCtrl'
       }
-    })
+    }
+  })
 
-    .state('tab.pet-detail', {
-      url: '/pet/:petId',
-      views: {
-        'pets-tab': {
-          templateUrl: 'templates/pet-detail.html',
-          controller: 'PetDetailCtrl'
-        }
+  .state('tab.pet-detail', {
+    url: '/pet/:petId',
+    views: {
+      'pets-tab': {
+        templateUrl: 'templates/pet-detail.html',
+        controller: 'PetDetailCtrl'
       }
-    })
+    }
+  })
 
-    .state('tab.adopt', {
-      url: '/adopt',
-      views: {
-        'adopt-tab': {
-          templateUrl: 'templates/adopt.html'
-        }
+  .state('tab.adopt', {
+    url: '/adopt',
+    views: {
+      'adopt-tab': {
+        templateUrl: 'templates/adopt.html'
       }
-    })
+    }
+  })
 
-    .state('tab.about', {
-      url: '/about',
-      views: {
-        'about-tab': {
-          templateUrl: 'templates/about.html',
-          controller:'Controller'
-        }
+  .state('tab.about', {
+    url: '/about',
+    views: {
+      'about-tab': {
+        templateUrl: 'templates/about.html',
+        controller: 'Controller'
       }
-    });
+    }
+  });
 
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/pets');
+  $urlRouterProvider.otherwise('/tab/about');
 
 });
-
--- js/controllers.js
+++ js/controllers.js
@@ -15,8 +15,88 @@ angular.module('starter.controllers', [])
 
 			$scope.master = {};
 
+			$scope.init = function() {
+
+				$scope.user.val1 = "111";
+				$scope.user.val0 = "222";
+				$scope.user.opt = [];
+				$scope.user.opt[0] = "111";
+				$scope.user.opt[1] = "222";
+				$scope.user.opt[2] = "333";
+				$scope.user.opt[3] = "444";
+				$scope.user.totalCorrect = 0;
+				$scope.user.totalCount = 0
+
+
+			};
+			$scope.getRandomInt = function(min, max) {
+				return Math.floor(Math.random() * (max - min + 1)) + min;
+
+			};
+			$scope.updateLimits = function() {
+				$scope.currentLimitIndex = $scope.currentLimitIndex + 1;
+				$scope.currentLimit = $scope.limitArray[$scope.currentLimitIndex];
+				$scope.maxCurrentLimit = $scope.currentLimit + $scope.currentLimit;
+
+			};
+
+
+
+			$scope.resetQuestion = function() {
+				var v1 = $scope.getRandomInt(0, $scope.currentLimit);
+				var v2 = $scope.getRandomInt(0, $scope.currentLimit);
+				$scope.user.val1 = v1;
+				$scope.user.val0 = v2;
+
+				var correctAnswerIndex = $scope.getRandomInt(0, 3);
+
+
+				for (var i = 0; i <= 3; i++) {
+
+					if (i == correctAnswerIndex) {
+
+						var temp = +v1 + +v2;
+						$scope.user.opt[i] = (temp);
+
+
+
+					} else {
+
+
+						var temp = $scope.getRandomInt(0, $scope.maxCurrentLimit);
+
+						$scope.user.opt[i] = temp;
+					}
+
+
+
+				}
+
+				if ($scope.user.totalCount == 180) {
+					alert("game over");
+				}
+				if ($scope.user.totalCount % 20 == 0) {
+					console.log("limit increase");
+					$scope.updateLimits();
+				}
+			};
+
+			$scope.checkAns = function(val) {
+
+				var ans = +$scope.user.val0 + +$scope.user.val1;
+
+				if (val == ans) {
+					//console.log("correct");
+					$scope.user.totalCorrect = +$scope.user.totalCorrect + 1;
+				}
+				$scope.user.totalCount = +$scope.user.totalCount + 1;
+
+				$scope.resetQuestion();
+
+			};
 			$scope.update = function(user) {
 				$scope.master = angular.copy(user);
+
 			};
 
 			$scope.reset = function() {
